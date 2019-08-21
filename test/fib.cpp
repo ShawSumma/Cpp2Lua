054ec0b0e759bd4418b1a524e3582e7dc9195e5d
Shaw Summa <shawsumma@gmail.com>
2019-08-20 21:38:15 -0400

    upload

--- test/fib.cpp
+++ test/fib.cpp
@@ -4,11 +4,12 @@ namespace bench {
     int fib(int x) {
         if (x < 2) {
             return x;
-        } 
+        }
         return fib(x-2) + fib(x-1);
     }
 }
 
 int main(void) {
-    lua::println(bench::fib(35));
+    using namespace lua;
+    println(bench::fib(33));
 }
---
+++ test/lambda.cpp
@@ -0,0 +1,32 @@
+#include <lua/io>
+
+using namespace lua;
+
+template <typename T>
+T fib(T n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib<T>(n-2) + fib<T>(n-1);
+}
+
+template <typename T>
+auto math(T a) {
+    return [=](T b) {
+        return [&](T c) {
+            return [=](T d) -> T {
+                return a * b + c * d;
+            };
+        };
+    };
+}
+
+int main() {
+    int arr[2][2] = {
+        {fib(6), fib(3)},
+        {fib(5), fib(10)}
+    };
+    auto val = math<long>(arr[0][0])(arr[0][1])(arr[1][0])(arr[1][1]);
+    println(val);
+    return 0;
+}
